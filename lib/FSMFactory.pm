f296fe8afc8c3eb4c15e034a5e6b2f4bc2526141
Denis T <dev@denis-it.com>
2017-02-07 14:12:21 +0300

    InstructorFSMController extracted to separated module

--- lib/FSMFactory.pm
+++ lib/FSMFactory.pm
@@ -8,12 +8,11 @@ use DateTimeFactory;
 use UserFSM;
 use UserFSMController;
 use InstructorFSM;
+use InstructorFSMController;
 use Instructors;
 use Localization qw(lz dt);
 use Resources;
 use RecordTimeParser;
-
-use DumperUtils;
 
 sub new {
 	my ($class, $api, $groups, $config) = @_;
@@ -50,97 +49,13 @@ sub create {
 
 sub instructor_fsm {
 	my ($self, $user, $chat_id) = @_;
-
-	InstructorFSM->new(
-		send_start_message => sub {
-			$self->{api}->send_message(
-				{chat_id => $chat_id, text => lz("instructor_start")});
-		},
-
-		send_cancel_message => sub {
-			$self->{api}->send_message({chat_id => $chat_id,
-					text => lz("instructor_operation_cancelled")});
-		},
-
-		send_menu => sub {
-			my @keyboard = (
-				lz("instructor_show_schedule"),
-				lz("instructor_add_record"),
-			);
-			$self->{api}->send_keyboard({
-				chat_id => $chat_id,
-				text => lz("instructor_menu"),
-				keyboard => \@keyboard
-			});
-		},
-
-		is_schedule_selected => sub {
-			my ($text) = @_;
-			$text eq lz("instructor_show_schedule");
-		},
-
-		is_add_record_selected => sub {
-			my ($text) = @_;
-			$text eq lz("instructor_add_record");
-		},
-
-		send_schedule => sub {
-			$self->{api}->send_message(
-				{chat_id => $chat_id, text => lz("instructor_schedule")});
-
-			my $instructor = $self->{instructors}->name($user->{id});
-			my $schedule = $self->{resources}->schedule($instructor);
-
-			my $text = "";
-			foreach my $resource (keys %$schedule) {
-				$text .= $resource . ":\n";
-				foreach my $event (@{$schedule->{$resource}}) {
-					$text .= dt($event->{span}->start) . " - ";
-					$text .= dt($event->{span}->end) . ": ";
-					$text .= ($event->{busy} ? lz("busy") : lz("free")) . "\n";
-				}
-			}
-
-			if ($text eq "") {
-				$text = lz("instructor_schedule_is_empty")
-			}
-
-			$self->{api}->send_message({chat_id => $chat_id, text => $text});
-		},
-
-		ask_record_time => sub {
-			my @keyboard = (
-				lz("instructor_cancel_operation"),
-			);
-			$self->{api}->send_keyboard({
-				chat_id => $chat_id,
-				text => lz("instructor_record_time"),
-				keyboard => \@keyboard
-			});
-		},
-
-		is_cancel_operation_selected => sub {
-			my ($text) = @_;
-			$text eq lz("instructor_cancel_operation");
-		},
-
-		ask_record_time_failed => sub {
-			$self->{api}->send_message({
-				chat_id => $chat_id, text => lz("invalid_record_time")});
-		},
-
-		parse_record_time => sub {
-			my ($text) = @_;
-			$self->{recordtimeparser}->parse($text);
-		},
-
-		save_record => sub {
-			my ($record) = @_;
-
-			$self->{api}->send_message({
-				chat_id => $chat_id, text => DumperUtils::span2str($record)});
-		},
-	);
+	InstructorFSM->new(InstructorFSMController->new(
+			$user,
+			$chat_id,
+			$self->{api},
+			$self->{instructors},
+			$self->{resources},
+			$self->{recordtimeparser}));
 }
 
 sub user_fsm {
---
+++ lib/InstructorFSMController.pm
@@ -0,0 +1,117 @@
+package InstructorFSMController;
+
+use strict;
+use warnings;
+
+use DumperUtils;
+
+sub new {
+	my ($class, $user, $chat_id, $api, $instructors, $resources, $recordtimeparser) = @_;
+	my $self = {
+		user => $user,
+		chat_id => $chat_id,
+
+		api => $api,
+		instructors => $instructors,
+		resources => $resources,
+		recordtimeparser => $recordtimeparser,
+
+		dtf => DateTimeFactory->new,
+		log => Log->new("instructorfsmcontroller"),
+	};
+	bless $self, $class;
+}
+
+sub _send_message {
+	my ($self, $text) = @_;
+	$self->{api}->send_message({
+		chat_id => $self->{chat_id},
+		text => $text
+	});
+}
+
+sub _send_keyboard {
+	my ($self, $text, $keyboard) = @_;
+	$self->{api}->send_keyboard({
+		chat_id => $self->{chat_id},
+		text => $text,
+		keyboard => $keyboard
+	});
+}
+
+sub send_start_message {
+	$self->_send_message(lz("instructor_start"));
+}
+
+sub send_cancel_message {
+	$self->_send_message(lz("instructor_operation_cancelled"));
+}
+
+sub send_menu {
+	my @keyboard = (
+		lz("instructor_show_schedule"),
+		lz("instructor_add_record"),
+	);
+	$self->_send_keyboard(lz("instructor_menu"), \@keyboard);
+}
+
+sub is_schedule_selected {
+	my ($text) = @_;
+	$text eq lz("instructor_show_schedule");
+}
+
+sub is_add_record_selected {
+	my ($text) = @_;
+	$text eq lz("instructor_add_record");
+}
+
+sub send_schedule {
+	$self->_send_message(lz("instructor_schedule"));
+
+	my $instructor = $self->{instructors}->name($api->{user}->{id});
+	my $schedule = $self->{resources}->schedule($instructor);
+
+	my $text = "";
+	foreach my $resource (keys %$schedule) {
+		$text .= $resource . ":\n";
+		foreach my $event (@{$schedule->{$resource}}) {
+			$text .= dt($event->{span}->start) . " - ";
+			$text .= dt($event->{span}->end) . ": ";
+			$text .= ($event->{busy} ? lz("busy") : lz("free")) . "\n";
+		}
+	}
+
+	if ($text eq "") {
+		$text = lz("instructor_schedule_is_empty");
+	}
+
+	$self->_send_message($text);
+}
+
+sub ask_record_time {
+	my @keyboard = (
+		lz("instructor_cancel_operation"),
+	);
+	$self->_send_keyboard(lz("instructor_record_time"), \@keyboard);
+}
+
+sub is_cancel_operation_selected {
+	my ($text) = @_;
+	$text eq lz("instructor_cancel_operation");
+}
+
+sub ask_record_time_failed {
+	$self->_send_message(lz("invalid_record_time"));
+}
+
+sub parse_record_time {
+	my ($text) = @_;
+	$self->{recordtimeparser}->parse($text);
+}
+
+sub save_record {
+	my ($record) = @_;
+	$self->_send_message(DumperUtils::span2str($record));
+}
+
+1;
