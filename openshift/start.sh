1244133f1449be6503fdb9e97ca40a0dd67a6fd9
Denis T <dev@denis-it.com>
2017-02-08 15:59:53 +0300

    small changes in openshift start script

--- openshift/start.sh
+++ openshift/start.sh
@@ -9,6 +9,7 @@ git stash pop
 cpanm -n -l $OPENSHIFT_DATA_DIR/perl5 --installdeps .
 
 killall -9 httpd
+killall -9 node
 node $OPENSHIFT_REPO_DIR/bookingbot/openshift/monitor.js &>> $OPENSHIFT_LOG_DIR/monitor.log &
 
-perl -I$OPENSHIFT_DATA_DIR/perl5/lib/perl5 -I$OPENSHIFT_REPO_DIR/bookingbot $OPENSHIFT_REPO_DIR/bookingbot/bot.pl &>> $OPENSHIFT_LOG_DIR/bookingbot.log &
+perl -I$OPENSHIFT_DATA_DIR/perl5/lib/perl5 -I$OPENSHIFT_REPO_DIR/serikoff.lib -I$OPENSHIFT_REPO_DIR/bookingbot $OPENSHIFT_REPO_DIR/bookingbot/bot.pl &>> $OPENSHIFT_LOG_DIR/bookingbot.log &
