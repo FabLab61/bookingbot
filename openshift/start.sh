16a6cf8d8436ed55a514993395d27cf4a42ed3d9
Denis T <dev@denis-it.com>
2017-02-09 12:48:12 +0300

    openshift start script improved again

--- openshift/start.sh
+++ openshift/start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+killall -9 httpd
+killall -9 node
+killall -9 perl
+
 set -e
 
 cd $OPENSHIFT_REPO_DIR/bookingbot 
@@ -12,9 +16,6 @@ git stash pop
 
 cpanm -n -l $OPENSHIFT_DATA_DIR/perl5 --installdeps .
 
-killall -9 httpd
-killall -9 node
-killall -9 perl
 node $OPENSHIFT_REPO_DIR/bookingbot/openshift/monitor.js &>> $OPENSHIFT_LOG_DIR/monitor.log &
 
 perl -I$OPENSHIFT_DATA_DIR/perl5/lib/perl5 -I$OPENSHIFT_REPO_DIR/serikoff.lib -I$OPENSHIFT_REPO_DIR/bookingbot $OPENSHIFT_REPO_DIR/bookingbot/bot.pl &>> $OPENSHIFT_LOG_DIR/bookingbot.log &
